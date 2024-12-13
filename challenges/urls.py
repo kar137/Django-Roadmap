4324b3dc0ff0eebf95a3e04477cf84133d1273bb
karanDevX11 <bistakaran89@gmail.com>
2024-12-13 09:42:47 +0545

    concept of dynamic path segment in views and urls

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 #these are the url patterns for this app named challenges
 urlpatterns = [
-    path('january', views.index),
+    path('<month>', views.monthly_challenges),  # these angular brackets provides a dyanmic path segment
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse   #imported http module
+from django.http import HttpResponse, HttpResponseNotFound   #imported http module
 # Create your views here.
 
-def index(request):  #this is a function based view processing request
-    return HttpResponse("This is working!!")   #returning with a response
+def monthly_challenges(request, month):  #this is a function based view processing request
+    challenge_text = None
+    if month == 'january':     #if and elif statements
+        challenge_text = "Walk 20 minutes in the park for everyday."
+    elif month == 'february':
+        challenge_text = "Practice 1hr of django everyday."
+    elif month == 'march':
+        challenge_text = "Eat Vegan diet everyday."
+    elif month == 'april':
+        challenge_text = "Complete atleast 3 selfhelp books."
+    else:
+        return HttpResponseNotFound("Page not found!")
+    return HttpResponse(challenge_text)   #returns a response to the server after processing the view
