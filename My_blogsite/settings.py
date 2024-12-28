19b6a829d616047884abd90b9e31b7eb87345e9b
karanDevX11 <bistakaran89@gmail.com>
2024-12-28 16:09:28 +0545

    update templates and static files

--- My_blogsite/settings.py
+++ My_blogsite/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [     #links static folder so the django looks into the root directory into static folder for static files
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
