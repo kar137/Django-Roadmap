2a92f3e7f154c46d6cd9b9688f6c681ced7af7d7
karanDevX11 <bistakaran89@gmail.com>
2024-12-29 14:55:36 +0545

    update all posts and style

--- Week-3/My_blogsite/blog/views.py
+++ Week-3/My_blogsite/blog/views.py
@@ -1,22 +1,14 @@
 from django.shortcuts import render
 from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 
-blogposts = {
-    'post1': 'Why did putin refuse to marry?',
-    'post2': 'Why did putin refuse to marry?',
-    'post3': 'Why did putin refuse to marry?'
-}
 
 # Create your views here.
 
 def starting_page(request):
-    post_list = list(blogposts.keys())
-    return render(request, 'blog/index.html', {
-        'post_list' : post_list
-    })
+    return render(request, 'blog/index.html')
 
 def posts(request):
-    pass
+    return render(request, 'blog/all-posts.html')
 
 def post_detail(request):
     pass
