38f9a28873595e1f7f6d28dfab77e654e4a4bc0f
karanDevX11 <bistakaran89@gmail.com>
2025-01-04 23:42:05 +0545

    implement different kind of relationships

--- Week-2/Book_Store/book_outlet/models.py
+++ Week-2/Book_Store/book_outlet/models.py
@@ -5,12 +5,47 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Countries"
+
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=10)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.street}, {self.postal_code}, {self.city}"
+    
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
+class Author(models.Model):
+    f_name = models.CharField(max_length=50)
+    l_name = models.CharField(max_length=50)
+
+    address = models.OneToOneField(Address, on_delete= models.CASCADE, null=True)
+
+    def full_name(self):
+        return f"{self.f_name} {self.l_name}"
+
+    def __str__(self):
+        return self.full_name()
+
 class Book(models.Model):
     title = models.CharField(max_length=50)  #charfield is a fieldtype to set title in database
     rating = models.IntegerField( validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", null=False, blank=True, db_index=True)
+
+    published_countries = models.ManyToManyField(Country, null=False)
 
     def get_absolute_url(self):
         return reverse("book-detail", args= [self.slug])
